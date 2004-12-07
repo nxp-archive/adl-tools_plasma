668b8d33fe39d63effd57e14c19f6fbef6dc7ef8
Brian Kahne <bkahne@freescale.com>
2004-12-07 22:07:52 +0000

    *** empty log message ***

---
+++ src/ChanSupport.C
@@ -0,0 +1,259 @@
+//
+// Various support classes for the channels.  Non-templated stuff should
+// be placed in here.
+//
+
+#include "Interface.h"
+#include "ChanSupport.h"
+
+using namespace std;
+
+namespace plasma {
+
+  /////////////// Timeout ///////////////
+
+  void timeout(void *a)
+  {
+    Timeout *to = (Timeout *)a;
+    pDelay(to->delay());
+    to->_ready = true;
+    pWake(make_pair(to->reset(),to->_h));
+  }
+
+  // This will sleep if we're not ready and will clear
+  // the ready state, "consuming" the timeout.
+  int Timeout::get() 
+  { 
+    pLock();
+    if (!ready()) {
+      pUnlock();
+      set_notify(pCurThread(),HandleType());
+      pLock();
+    }
+    _ready = false;
+    pUnlock();
+    return 0; 
+  };
+
+  THandle Timeout::clear_notify()
+  {
+    if (_writet) {
+      pTerminate(_writet);
+      _writet = 0;
+    }
+    return reset();
+  }
+
+  THandle Timeout::reset() 
+  { 
+    THandle t = _readt; 
+    _readt = 0;
+    return t; 
+  };
+
+  void Timeout::set_notify(plasma::THandle t,plasma::HandleType h)
+  {
+    _ready = false;
+    _readt = t;
+    _h = h;
+    assert(!_writet);
+    _writet = pSpawn(timeout,this,-1);
+  }
+
+  /////////////// ClockChan ///////////////
+
+  ClockChanImpl::ClockChanImpl(ptime_t p,ptime_t s,unsigned ms) : 
+    _period(p), _skew(s), _maxsize(ms), _size(0)
+  {}
+
+  // Returns true if we're on a clock edge, given a clock period.
+  bool ClockChanImpl::is_phi() const
+  {
+    return (pTime() % _period) == _skew;
+  }
+
+  // Returns the time of the next clock cycle.  We compute the time of the 
+  // last edge, then add on the skew (or another cycle if no skew was specified)
+  // to get the next edge.
+  ptime_t ClockChanImpl::next_phi() const
+  {
+    return ( (((pTime()+_period)-_skew) / _period )*_period)+_skew;
+  }
+
+  //
+  // Single-consumer support class.
+  //
+
+  SingleConsumerClockChannel::SingleConsumerClockChannel(ptime_t p,ptime_t s,unsigned ms) : 
+    ClockChanImpl(p,s,ms), _waket(0), _delay(0), _readt(0)
+  {}
+
+  TPair SingleConsumerClockChannel::reset() 
+  { 
+    THandle t = _readt; 
+    _readt = 0; 
+    _waket = 0;
+    return make_pair(t,_h);
+  }
+
+  void SingleConsumerClockChannel::set_notify(THandle t,HandleType h) 
+  { 
+    assert(!_readt); 
+    _readt = t; 
+    _h = h; 
+    // If we have data, start the waker.  We don't need to check whether
+    // the data is current b/c we wouldn't be here if it weren't.
+    if (!empty()) {
+      start_waker();
+    }
+  }
+
+  void SingleConsumerClockChannel::cancel_waker()
+  {
+    if (_waket) {
+      pTerminate(_waket);
+      _waket = 0;
+    }
+  }
+
+  void sc_delayed_waker(void *a)
+  {
+    SingleConsumerClockChannel *cc = (SingleConsumerClockChannel *)a;
+    pDelay(cc->delay());
+    pWake(cc->reset());
+  }
+
+  // Start a wake-up thread only if one doesn't already exist.
+  void SingleConsumerClockChannel::start_waker()
+  {
+    if (!_waket) {
+      _delay = next_phi() - pTime();
+      assert(!_waket);
+      _waket = pSpawn(sc_delayed_waker,this,-1);
+    }
+  }
+
+  void SingleConsumerClockChannel::delayed_wakeup(bool current_data)
+  {
+    if (is_phi() && current_data) {
+      // We're on a clock edge- wake up thread.
+      // Cancel a waker thread if it exists.
+      cancel_waker();
+      pWake(reset());
+    } else {
+      // Not on a clock edge- schedule a thread to
+      // wake up the reader at the correct time.
+      start_waker();
+    }
+  }
+
+  void SingleConsumerClockChannel::delayed_reader_wakeup(bool have_data)
+  {
+    // If we're not empty, and we're here, then it's because we're not on a clock
+    // edge- in that case we'll start a waker thread.
+    // If we are empty, then we're ready for notification when we do get data.
+    set_notify(pCurThread(),HandleType());
+    pSleep();
+  }
+
+  //
+  // Multi-consumer support class.
+  //
+
+  MultiConsumerClockChannel::MultiConsumerClockChannel(ptime_t p,ptime_t s,unsigned ms) : 
+    ClockChanImpl(p,s,ms)
+  {}
+
+  // Removes a consumer from the known set.
+  TPair MultiConsumerClockChannel::reset(MClkInfo::iterator iter) 
+  { 
+    TPair t = iter->second._tinfo;
+    _cons.erase(iter);
+    return t;
+  }
+
+  // Adds a consumer to the known set and sets up a waker thread if
+  // we have data.
+  void MultiConsumerClockChannel::set_notify(THandle t,HandleType h) 
+  { 
+    pair<MClkInfo::iterator,bool> ip = _cons.insert(std::make_pair(t,MClk(t,h)));
+    // If we have data, start the waker.  We don't need to check whether
+    // the data is current b/c we wouldn't be here if it weren't.
+    if (!empty()) {
+      start_waker(ip.first);
+    }
+  }
+
+  // This only cancels the waking thread if the current thread is
+  // equal to the pending reader thread.
+  void MultiConsumerClockChannel::clear_notify() 
+  { 
+    MClkInfo::iterator iter = _cons.find(pCurThread());
+    cancel_waker(iter);
+    _cons.erase(iter);
+  }
+
+  // Cancel a waker if the iterator is valid (not end()) and a waker
+  // exists for the thread.  Note:  This does not remove the consumer.
+  // You have to call reset to do that.
+  void MultiConsumerClockChannel::cancel_waker(MClkInfo::iterator iter)
+  {
+    if (iter != _cons.end() && iter->second._waker) {
+      pTerminate(iter->second._waker);
+      iter->second._waker = 0;
+    }
+  }
+
+  struct WakeArgs {
+    int                        _delay;
+    MClkInfo::iterator         _iter;
+    MultiConsumerClockChannel *_mc;
+
+    WakeArgs(MultiConsumerClockChannel *mc) : _mc(mc) {};
+  };
+
+  void mc_delayed_waker(void *a)
+  {
+    WakeArgs &args = *((WakeArgs*)a);
+    MultiConsumerClockChannel &mc = *(args._mc);
+    pDelay(args._delay);
+    pWake(mc.reset(args._iter));
+  }
+
+  // Start a wake-up thread only if one doesn't already exist for the
+  // specified thread.
+  void MultiConsumerClockChannel::start_waker(MClkInfo::iterator iter)
+  {
+    if (iter != _cons.end() && !iter->second._waker) {
+      WakeArgs args(this);
+      args._delay = next_phi() - pTime();
+      args._iter = iter;
+      iter->second._waker = pSpawn(mc_delayed_waker,sizeof(WakeArgs),&args,-1).first;
+    }
+  }
+
+  void MultiConsumerClockChannel::delayed_wakeup(bool current_data)
+  {
+    if (is_phi() && current_data) {
+      // We're on a clock edge- wake up thread.
+      // Cancel a waker thread if it exists.
+      cancel_waker(_cons.begin());
+      pWake(reset(_cons.begin()));
+    } else {
+      // Not on a clock edge- schedule a thread to
+      // wake up the reader at the correct time.
+      start_waker(_cons.begin());
+    }
+  }
+
+  void MultiConsumerClockChannel::delayed_reader_wakeup(bool have_data)
+  {
+    // If we're not empty, and we're here, then it's because we're not on a clock
+    // edge- in that case we'll start a waker thread.
+    // If we are empty, then we're ready for notification when we do get data.
+    set_notify(pCurThread(),HandleType());
+    pSleep();
+    clear_notify();
+  }
+
+}
---
+++ src/ChanSupport.h
@@ -0,0 +1,204 @@
+//
+// Various support classes for the channels.  Non-templated stuff should
+// be placed in here.
+//
+
+#ifndef _CHANSUPPORT_H_
+#define _CHANSUPPORT_H_
+
+#include <assert.h>
+#include <ext/hash_map>
+
+namespace plasma {
+
+  // Base class for channels that allow multiple producers.  An actual
+  // channel should inherit from this class and implement write, read, and get.
+  class MultiProducerChannel {
+    typedef std::vector<THandle,traceable_allocator<THandle> > Writers;
+  public:
+
+  protected:
+    void set_writenotify(THandle t) { _writers.push_back(t); };
+    bool have_writers() const { return !_writers.empty(); };
+    THandle next_writer() { THandle t = _writers.back(); _writers.pop_back(); return t; };
+
+    Writers    _writers;
+  };
+
+  // Base class for a simple channel that allows only one consumer but
+  // multiple producers.
+  class SingleConsumerChannel {
+  public:
+    SingleConsumerChannel() : _readt(0) {};
+
+    // These are marked as non-mutex b/c they are used by alt, which already
+    // does the locking.
+    void set_notify(THandle t,HandleType h) { assert(!_readt); _readt = t; _h = h; };
+    void clear_notify() { _readt = 0; };
+
+  protected:
+    // Do we have a waiting reader?
+    bool have_reader() const { return _readt; };
+    std::pair<THandle,HandleType> notify_reader() { THandle t = _readt; _readt = 0; return std::make_pair(t,_h); };
+
+  private:
+    THandle    _readt;
+    HandleType _h;
+  };
+
+  struct HashTHandle {
+    size_t operator()( THandle t ) const { return (size_t)t; };
+  };
+
+  typedef __gnu_cxx::hash_map<THandle,HandleType,HashTHandle> ConInfo;
+  typedef std::pair<THandle,HandleType> TPair;
+
+  // Base class for a simple channel that allows multiple consumers and multiple
+  // producers.  Note:  The order in which multiple consumers will be satisfied
+  // is non-deterministic.
+  class MultiConsumerChannel {
+
+  public:
+    MultiConsumerChannel(){};
+
+    // These are marked as non-mutex b/c they are used by alt, which already
+    // does the locking.
+    void set_notify(THandle t,HandleType h) { _cons.insert(std::make_pair(t,h)); };
+    // This clears the notification for the current thread- should only be called by
+    // reader threads.
+    void clear_notify() { _cons.erase(_cons.find(pCurThread())); };
+
+  protected:
+    // Do we have a waiting reader?
+    bool have_reader() const { return !_cons.empty(); };
+    // This gets the next reader in a non-deterministic fashion.
+    TPair notify_reader() { TPair t = *(_cons.begin()); _cons.erase(_cons.begin()); return t; };
+
+  private:
+    ConInfo _cons;
+  };
+
+  // Base class for single-data item channels.
+  class SingleDataChannelBase : public MultiProducerChannel {
+  public:
+    SingleDataChannelBase() : _ready(false) {};
+    bool ready() const { return _ready; };
+    bool full() const { return _ready; };
+    void clear_ready() { pLock(); _ready = false; pUnlock(); };
+
+  protected:
+    void set_ready(bool r) { _ready = r; };
+
+  private:
+    bool       _ready;
+  };
+
+  //
+  // Timeout channel:  When used in an alt block, will set ready after a specified
+  // amount of simulation time.
+  //
+  class Timeout {
+  public:
+    Timeout(ptime_t d) : _ready(false), _delay(d), _readt(0), _writet(0) {};
+
+    ptime_t delay() const { return _delay; };
+    void setDelay(ptime_t d) { _delay = d; };
+
+    bool ready() const { return _ready; };
+    int get();
+
+    // These are marked as non-mutex b/c they are used by alt, which already
+    // does the locking.
+    void set_notify(THandle t,HandleType h);
+    THandle clear_notify();
+
+  private:
+    friend void timeout(void *a);
+    THandle reset();
+
+    bool       _ready;
+    ptime_t    _delay;
+    THandle    _readt;
+    THandle    _writet;
+    HandleType _h;
+  };
+
+  // Non-templated implementation class used by ClockChan- do not use this
+  // directly.
+  class ClockChanImpl {
+  public:
+    ClockChanImpl(ptime_t p,ptime_t s,unsigned maxsize);
+
+    bool is_phi() const;
+    ptime_t next_phi() const;
+
+    bool empty() const { return _size == 0; };
+    unsigned size() const { return _size; };
+
+    unsigned maxsize() const { return _maxsize; };
+    void setMaxSize(unsigned ms) { _maxsize = ms; }
+    bool full() const { return _maxsize && _size >= _maxsize; };
+
+    void incr_size() { ++_size; };
+    void decr_size() { --_size; };
+    void check_size() const { assert(!_maxsize || _size <= _maxsize); }
+    
+  private:
+    ptime_t    _period;    // Clock period.
+    ptime_t    _skew;      // Skew- offset from clock edge.
+    unsigned   _maxsize;   // Max size.  If 0, no fixed size.
+    unsigned   _size;      // Number of items in channel.
+  };
+
+  // Base class used for single-consumer clocked channels.
+  class SingleConsumerClockChannel : public ClockChanImpl {
+  public:
+    SingleConsumerClockChannel(ptime_t p,ptime_t s,unsigned maxsize);
+
+    bool have_reader() const { return _readt; };
+    void set_notify(THandle t,HandleType h);
+    void clear_notify() { cancel_waker(); };
+    void delayed_wakeup(bool current_data);
+    void delayed_reader_wakeup(bool have_data);
+    void start_waker();
+    TPair reset();
+    void cancel_waker();
+    int delay() const { return _delay; };
+
+  private:
+    THandle    _waket;     // Thread which wakes reader at the correct time.
+    int        _delay;     // Stores delay time for waker.
+    THandle    _readt;     // Read thread.
+    HandleType _h;
+  };
+
+  struct MClk {
+    TPair      _tinfo;
+    THandle    _waker;
+
+    MClk(THandle t,HandleType h) : _tinfo(t,h), _waker(0) {};
+  };
+  typedef __gnu_cxx::hash_map<THandle,MClk,HashTHandle> MClkInfo;
+
+  // Base class used for multi-consumer clocked channels.
+  class MultiConsumerClockChannel : public ClockChanImpl {
+  public:
+    MultiConsumerClockChannel(ptime_t p,ptime_t s,unsigned maxsize);
+
+    bool have_reader() const { return !_cons.empty(); };
+    void set_notify(THandle t,HandleType h);
+    void clear_notify();
+    void cancel_waker(MClkInfo::iterator iter);
+    // This wakes up the next available reader.
+    void delayed_wakeup(bool current_data);
+    void delayed_reader_wakeup(bool have_data);
+    void start_waker(MClkInfo::iterator);
+    TPair reset(MClkInfo::iterator);
+
+  private:
+    MClkInfo    _cons;
+  };
+
+}
+
+#endif
